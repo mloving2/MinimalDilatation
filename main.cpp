cbd0bbd9714915a94b7da2df6d4c7739350d8834
Marissa Loving <marissaloving@Marissas-MacBook-Air.local>
2017-07-11 23:28:40 -0500

    second update: added weighted edges

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@
 //
 
 
-// C++ program to print DFS traversal from a given vertex in a  given graph
 #include<iostream>
 #include <fstream>
 #include<list>
 #include<vector>
 #include <string>
+#include <stack>
 
 using namespace std;
 
@@ -22,104 +22,47 @@ class Graph
 {
     int V;    // No. of vertices
     list<int> *adj;    // Pointer to an array containing adjacency lists
+    vector<pair<int,vector<int>>> *adj2;
     void DFSUtil(int v, bool visited[]);  // A function used by DFS
     bool isCyclicUtil(int v, bool visited[], bool *rs);  // used by isCyclic()
 public:
     Graph(int V);   // Constructor
-    void addEdge(int v, int w);   // function to add an edge to graph
-    void DFS(int v);    // DFS traversal of the vertices reachable from v
-    //void Cycle(int v);  //cycle based at v
-    bool isCyclic();    // returns true if there is a cycle in this graph
+    void addEdge(int v, int w, vector<int>); // function to add an edge to graph
+    void iterativeDFS(int v);
 };
 
 Graph::Graph(int V)
 {
     this->V = V;
     adj = new list<int>[V];
+    adj2 = new vector<pair<int,vector<int>>>[V];
 }
 
-void Graph::addEdge(int v, int w)
+void Graph::addEdge(int v, int w, vector<int> M)
 {
-    adj[v].push_back(w); // Add w to v’s list.
-}
-
-void Graph::DFSUtil(int v, bool visited[])
-{
-    // Mark the current node as visited and print it
-    visited[v] = true;
-    cout << v << " ";
+    adj[v].push_back(w);
+    adj2[v].push_back(make_pair(w, M));
     
-    // Recur for all the vertices adjacent to this vertex
-    list<int>::iterator i;
-    for (i = adj[v].begin(); i != adj[v].end(); ++i)
-        if (!visited[*i])
-            DFSUtil(*i, visited);
 }
 
-// DFS traversal of the vertices reachable from v.
-// It uses recursive DFSUtil()
-void Graph::DFS(int v)
-{
-    // Mark all the vertices as not visited
-    bool *visited = new bool[V];
-    for (int i = 0; i < V; i++)
-        visited[i] = false;
+void Graph::iterativeDFS(int v){
+    stack<int> S;
+    int top;
+    vector<bool> Visited(V, false);
     
-    // Call the recursive helper function to print DFS traversal
-    DFSUtil(v, visited);
-}
-
-// This function is a variation of DFSUytil() in http://www.geeksforgeeks.org/archives/18212
-bool Graph::isCyclicUtil(int v, bool visited[], bool *recStack)
-{
-    if(visited[v] == false)
-    {
-        // Mark the current node as visited and part of recursion stack
-        visited[v] = true;
-        recStack[v] = true;
-        
-        // Recur for all the vertices adjacent to this vertex
-        list<int>::iterator i;
-        for(i = adj[v].begin(); i != adj[v].end(); ++i)
-        {
-            if ( !visited[*i] && isCyclicUtil(*i, visited, recStack) )
-                return true;
-            else if (recStack[*i])
-                return true;
+    S.push(v);
+    
+    while(!S.empty()){
+        top = S.top();
+        S.pop();
+        if(Visited[v] == false){
+            Visited[v] = true;
         }
-        
+        for (auto i = adj[top].begin(); i != adj[top].end(); ++i)
+            if (!Visited[*i])
+                S.push(*i);
     }
-    recStack[v] = false;  // remove the vertex from recursion stack
-    return false;
 }
-
-// Returns true if the graph contains a cycle, else false.
-// This function is a variation of DFS() in http://www.geeksforgeeks.org/archives/18212
-bool Graph::isCyclic()
-{
-    // Mark all the vertices as not visited and not part of recursion
-    // stack
-    bool *visited = new bool[V];
-    bool *recStack = new bool[V];
-    for(int i = 0; i < V; i++)
-    {
-        visited[i] = false;
-        recStack[i] = false;
-    }
-    
-    // Call the recursive helper function to detect cycle in different
-    // DFS trees
-    for(int i = 0; i < V; i++)
-        if (isCyclicUtil(i, visited, recStack))
-            return true;
-    
-    return false;
-}
-
-/*void Graph::Cycle(int v){
-    
-}*/
-
 
 
 int main()
@@ -186,10 +129,6 @@ int main()
     
     infile.close();
     
-    cout << "Number of vertices: " << numberOfVertices << '\n';
-    
-    cout << "Maximal Valence: " << maxValence << '\n';
-    
     Graph scratchGraph(numberOfVertices);
     
     infile.open("/Users/marissaloving/Downloads/scratch.txt");
@@ -208,6 +147,8 @@ int main()
         
         while (!infile.eof()){
             
+            vector<int> M;
+            
             for(int j = 0; j < uniqueVertices.size(); j++){
                 if(uniqueVertices[j] == adjacentVertex){
                     tV = j;
@@ -217,29 +158,28 @@ int main()
                 }
             }
             
+            for (int i = 0; i < adjacentMatrix.size();i++){
+                if (adjacentMatrix[i] == '1'){
+                    M.push_back(1);
+                }
+                if (adjacentMatrix[i] == '0'){
+                    M.push_back(0);
+                }
+            }
+
+            
             infile >> firstVertex;
             infile >> adjacentVertex;
             getline(infile, adjacentMatrix);
             
-            //cout << "Initial vertex: " << iV << '\n';
-            //cout << "Terminal vertex: " << tV << '\n';
             
-            
-            scratchGraph.addEdge(iV, tV);
+            scratchGraph.addEdge(iV, tV, M);
             
         }
     }
     
     infile.close();
     
-// output
-    
-    if(scratchGraph.isCyclic()){
-        cout << "Graph contains cycle" << '\n';
-    }
-    else{
-        cout << "Graph doesn't contain cycle" << '\n';
-    }
     
     return 0;
 }
